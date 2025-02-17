dfc0773d50a2dc8224dce84a30627d52c702ffec
Abderrahmane <abderrahmanesaghraoui@gmail.com>
2025-02-17 18:50:20 +0100

    add files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -33,7 +36,14 @@ function App() {
       <div>
             <WelcomeMessage />
         </div>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
     </>
+
+
   )
 }
 
