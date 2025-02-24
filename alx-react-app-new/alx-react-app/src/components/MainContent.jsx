c335e99a71ee67d2ac89cbd6f8cb83c438199d69
Abderrahmane <abderrahmanesaghraoui@gmail.com>
2025-02-24 17:19:53 +0000

    modify file

---
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#eaeaea', minHeight: '400px', textAlign: 'center' }}>
+            <p style={{ fontSize: '1.2rem', color: '#333', lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
