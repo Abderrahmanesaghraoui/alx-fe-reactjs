23e3679541b7c33617ef8719705ba4b99f8309b8
Abderrahmanesaghraoui <abderrahmanesaghraoui@gmail.com>
2025-02-24 17:53:56 +0000

    Delete alx-react-app-new/alx-react-app directory

--- alx-react-app-new/alx-react-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
-            <p style={{ margin: 0, fontSize: '1rem' }}>© 2023 City Lovers</p>
-        </footer>
-    );
-}
-
-export default Footer;
