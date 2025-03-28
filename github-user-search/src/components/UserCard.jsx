086b8752e4b0edcc0279138aed58f06a60e2441f
Abderrahmane <abderrahmanesaghraoui@gmail.com>
2025-03-28 01:21:05 +0000

    add file

--- github-user-search/src/components/UserCard.jsx
+++ github-user-search/src/components/UserCard.jsx
@@ -2,14 +2,11 @@ const UserCard = ({ user }) => {
     return (
       <div className="user-card">
         <img 
-          src={user.avatar_url} 
-          alt={`${user.login}'s avatar`} 
-          className="avatar"
+          src={user.avatar_url}  // Displays avatar
+          alt={user.login}       // Uses login as alt text
         />
-        <h3>{user.login}</h3>
-        <a href={user.html_url} target="_blank" rel="noopener noreferrer">
-          View Profile
-        </a>
+        <h2>{user.login}</h2>    // Shows username
+        <a href={user.html_url}>View Profile</a>
       </div>
     );
   };
